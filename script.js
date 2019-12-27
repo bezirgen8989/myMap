a0d0f5049ed84f34641d3c24914afbb591f7a81a
Bezirgen <bezirgen8989@mail.ru>
2019-12-27 15:40:40 +0300

    пытался поменять цвет точки

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ ymaps.ready(init);
 
 function init(){
     var myMap = new ymaps.Map("map", {
-        center: [53.132341, 26.017609], // // Координаты центра карты: «широта, долгота».
-        zoom: 12, // Уровень масштабирования 0...19
+        center: [53.118287519761, 26.023592998577826], // Координаты центра карты: «широта, долгота».
+        zoom: 15, // Уровень масштабирования 0...19
         controls: ['zoomControl'], // для работы с картой 
         // behaviors: [] // для работы с пользовательскими событиями 
-    });
+    }); 
+    ymaps.modules.require(['pieChartCluster'], function (pieChartCluster){
+        var clusters = new pieChartCluster();
+        var point = [
+            new ymaps.Placemark(
+                [53.11752533602894,26.02424972876957],
+                {
+                    preset: 'islands#greyIcon'
+                }
+            )
+        ]
+    })
 
     var myGeoObject = new ymaps.GeoObject({
         geometry: {
@@ -14,8 +25,23 @@ function init(){
             preset: 'islands#redIcon',
             coordinates: [53.118287519761, 26.023592998577826] // координаты точки
         }
-
     });
+
+            
     // Размещение геообъекта на карте.
+    
     myMap.geoObjects.add(myGeoObject); 
+    clusterer.add(points);
+    myMap.geoObjects.add(clusters); 
+
+   
 }
+
+
+
+// objectManager.objects.setObjectOptions(1, {
+//     preset: 'islands#redIcon',
+//     balloonContentBody: 'Загружено (бла бла бла)',
+//     properties: {
+//         balloonContentBody: 'Загружено (бла бла бла)'
+// }
