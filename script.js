130e1f632746fb4ea6c4d99133957e7c5062911e
Bezirgen <bezirgen8989@mail.ru>
2019-12-30 15:05:44 +0300

    изменил цвета точек и добавил значки

--- script.js
+++ script.js
@@ -7,41 +7,28 @@ function init(){
         controls: ['zoomControl'], // для работы с картой 
         // behaviors: [] // для работы с пользовательскими событиями 
     }); 
-    ymaps.modules.require(['pieChartCluster'], function (pieChartCluster){
-        var clusters = new pieChartCluster();
-        var point = [
-            new ymaps.Placemark(
-                [53.11752533602894,26.02424972876957],
-                {
-                    preset: 'islands#greyIcon'
-                }
-            )
-        ]
-    })
 
     var myGeoObject = new ymaps.GeoObject({
         geometry: {
             type: "Point", // точка
-            preset: 'islands#redIcon',
             coordinates: [53.118287519761, 26.023592998577826] // координаты точки
+        },
+        
+    },{preset: 'islands#bluePocketIcon'}
+    );
+
+    var myGeoObject1 = new ymaps.GeoObject({
+        geometry: {
+            type: "Point", // точка
+            coordinates: [53.11752533602894,26.02424972876957], // координаты точки
+            balloonContentBody: 'БеларусьБанк'
         }
-    });
-
-            
-    // Размещение геообъекта на карте.
+    }, 
+    {preset:'islands#redMoneyIcon'}
+    );
     
-    myMap.geoObjects.add(myGeoObject); 
-    clusterer.add(points);
-    myMap.geoObjects.add(clusters); 
+    // Размещение геообъекта на карте.
+    myMap.geoObjects.add(myGeoObject);
+    myMap.geoObjects.add(myGeoObject1);
 
-   
 }
-
-
-
-// objectManager.objects.setObjectOptions(1, {
-//     preset: 'islands#redIcon',
-//     balloonContentBody: 'Загружено (бла бла бла)',
-//     properties: {
-//         balloonContentBody: 'Загружено (бла бла бла)'
-// }
