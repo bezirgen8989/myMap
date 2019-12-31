2d3e77e192957416bb67fe9a36f1e1fc848cff4e
Bezirgen <bezirgen8989@mail.ru>
2019-12-31 12:14:56 +0300

    добавил точку магазина, научился делать баллуны

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function init(){
     var myGeoObject = new ymaps.GeoObject({
         geometry: {
             type: "Point", // точка
-            coordinates: [53.118287519761, 26.023592998577826] // координаты точки
+            coordinates: [53.118287519761, 26.023592998577826], // координаты точки
         },
         
     },{preset: 'islands#bluePocketIcon'}
@@ -21,14 +21,24 @@ function init(){
         geometry: {
             type: "Point", // точка
             coordinates: [53.11752533602894,26.02424972876957], // координаты точки
-            balloonContentBody: 'БеларусьБанк'
         }
     }, 
     {preset:'islands#redMoneyIcon'}
     );
     
+    myPlacemark = new ymaps.Placemark([53.11920657146598,26.027836888850103], {
+        // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+        balloonContentHeader: "Балун метка",
+        balloonContentBody: "Содержимое <em>балуна</em> метки",
+        balloonContentFooter: "Подвал",
+        hintContent: "Хинт метки"
+    },
+    {preset: 'islands#oliveShoppingIcon'}
+    );
+
     // Размещение геообъекта на карте.
     myMap.geoObjects.add(myGeoObject);
     myMap.geoObjects.add(myGeoObject1);
+    myMap.geoObjects.add(myPlacemark);
 
 }
